3e5a4fc797532f0e8a2c95ac4e5b07a4cd6c3ca0
rmpestano <bigboss666>
2017-03-11 14:38:14 -0300

    perf

--- app/src/main/java/com/adminfaces/github/admin_app/MainActivity.java
+++ app/src/main/java/com/adminfaces/github/admin_app/MainActivity.java
@@ -15,7 +15,7 @@ import android.webkit.WebView;
 public class MainActivity extends AppCompatActivity {
 
     private static ViewGroup webViewParentViewGroup = null;
-    private WebView webView = null;
+    private static WebView webView = null;
     //private String context = "http://10.0.2.2:8080/showcase"; //localhost
     private String context =  "http://adminfaces-rpestano.rhcloud.com/showcase/";
 
